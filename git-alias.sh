6f0992dfac8c825f117b8e5bb3363aec9e254a4a
xingfeng-1992 <xingfeng.unsw@gmail.com>
2019-03-05 01:14:54 +1100

    update

---
+++ git-alias.sh
@@ -0,0 +1,7 @@
+git config --global alias.s status
+git config --global alias.ph push
+git config --global alias.pl pull
+git config --global alias.d diff
+git config --global alias.a add
+git config --global alias.c commit
+git config --global alias.ac '!git add -A && git commit -m'
