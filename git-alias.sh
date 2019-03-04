5ffe9be1e3e9ececc8fab7f51ff078e8c1c6c67e
xingfeng-1992 <xingfeng.unsw@gmail.com>
2019-03-05 01:16:47 +1100

    update

--- git-alias.sh
+++ git-alias.sh
@@ -5,3 +5,4 @@ git config --global alias.d diff
 git config --global alias.a add
 git config --global alias.c commit
 git config --global alias.ac '!git add -A && git commit -m'
+git config --global alias.l log
